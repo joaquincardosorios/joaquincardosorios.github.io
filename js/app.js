3ec6401b1f361302a807a53269dd89203ea8f46b
Joaquin Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-11 14:25:11 -0400

    export/print-option-on-progress-button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 function iniciarApp() {
 
     const selectCategorias = document.querySelector('#categorias');
-    selectCategorias.addEventListener('change', seleccionarCategoria)
+    if(selectCategorias){
+        selectCategorias.addEventListener('change', seleccionarCategoria);
+        obtenerCategorias();
+    }
 
     const resultado = document.querySelector('#resultado');
     const modal = new bootstrap.Modal('#modal', {})
-
-    obtenerCategorias();
 
     function obtenerCategorias() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
@@ -178,7 +179,10 @@ function iniciarApp() {
         //Boton Exportar PDF
         const btnExportar = document.createElement('BUTTON');
         btnExportar.classList.add('btn', 'btn-secondary', 'col');
-        btnExportar.textContent = 'Exportar';
+        btnExportar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+        <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+        <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
+      </svg> Imprimir`;
         btnExportar.onclick = function () {
             const modalContent = document.querySelector('#modal').innerHTML;
             const printWindow = window.open('', '', 'height=400,width=800');
