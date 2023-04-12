7b96fd34f8c48e32d44895d9da2f267e19eea5a4
Joaquin Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-12 08:29:26 -0400

    add-alert-if-empty

--- proyectos/buscador/js/app.js
+++ proyectos/buscador/js/app.js
@@ -10,13 +10,31 @@ function validarFormulario(e){
 
     const terminoBusqueda = document.querySelector('#termino').value;
     if (terminoBusqueda === ''){
-        console.log('Agrega un termino de busqueda');
+        mostrarAlerta('Agrega un termino de busqueda');
         return;
     }
 
 }
 
 function mostrarAlerta(mensaje) {
-    const alerta = document.createElement('P');
-    alerta.classList.add()
+
+    const existeAlerta = document.querySelector('.bg-danger');
+
+    if (!existeAlerta){
+        const alerta = document.createElement('P');
+        alerta.classList.add('bg-danger', 'border', 'px-4', 'py-3', 'rounded', 'col-5', 'mx-auto', 'mt-3', 'text-center', 'text-white');
+
+        alerta.innerHTML= `
+            <strong class="d-block fw-bold">Error!</strong>
+            <span class="block d-sm-inline">${mensaje}</span>
+        `;
+        formulario.appendChild(alerta)
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+
+    }
+
+    
 }
