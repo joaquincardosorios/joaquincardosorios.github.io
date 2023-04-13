43a9c7d6eb3871c15fbd0a3932436197810f1986
Joaquin Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-13 10:10:48 -0400

    spinner

--- proyectos/javascript/cryptoCotizador/js/app.js
+++ proyectos/javascript/cryptoCotizador/js/app.js
@@ -80,6 +80,8 @@ function consultarAPI(){
     const {moneda, criptomoneda} = objBusqueda;
     url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
+    mostrarSpinner()
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(cotizacion => {
@@ -119,4 +121,19 @@ function limpiarHTML(){
     while(resultado.firstChild){
         resultado.removeChild(resultado.firstChild)
     }
+}
+
+function mostrarSpinner(){
+    limpiarHTML();
+
+    const spinner = document.createElement('DIV');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `
+
+    resultado.appendChild(spinner)
 }
